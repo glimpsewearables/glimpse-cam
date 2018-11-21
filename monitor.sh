65d1cb683a6d3676f9ed4e4e3756769579443d08
jungo3 <38149377+jungo3@users.noreply.github.com>
2018-11-20 17:54:35 -0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -4,6 +4,6 @@ do
 	uptime -p >> battery.csv
 	date >> battery.csv
 	hostname -I >> battery.csv
-	python -c "import tinys3, socket, sys; sys.path.insert(0, '/home/pi/glimpse-cam'); import getLines as decode; A=decode.retKey(); conn=tinys3.Connection(A[0],A[1],tls=True,default_bucket='pi-1'); f=open('battery.csv','rb'); conn.upload(socket.gethostname()+'/battery.csv',f);"
+	python -c "import tinys3, socket, sys; sys.path.insert(0, '/home/pi/glimpse-cam'); import getLines as decode; A=decode.retKey(); conn=tinys3.Connection(A[0],A[1],tls=True,default_bucket='pi-5'); f=open('battery.csv','rb'); conn.upload(socket.gethostname()+'/battery.csv',f);"
 	sleep 60
 done
