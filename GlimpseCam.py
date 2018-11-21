f27c60eb6f169749714ef6f510c23004cbcc0953
jungo3 <38149377+jungo3@users.noreply.github.com>
2018-11-20 18:02:11 -0800

    Update GlimpseCam.py

--- GlimpseCam.py
+++ GlimpseCam.py
@@ -3,15 +3,20 @@
 import time, datetime, os, glob, socket
 import RPi.GPIO as GPIO
 import subprocess as sub
+from logger import log
 
+# Sets up log
+jack = log("errorLog", False).getlogger()
+jack.info("Device started.")
+
+# Initialize variables
 currentState = False
 prevState = False
 picture = True
-filename = "file.wav"
-held = False
+filename = ""
 backlogUploadTime = time.time()
 
-
+# Start pikrellcam and directory watcher
 sub.call('python /home/pi/glimpse-cam/uploadFile.py &',shell=True)
 sub.call('/home/pi/pikrellcam/pikrellcam &',shell=True)
 time.sleep(3)
@@ -44,6 +49,8 @@ time.sleep(3)
 	#INSERT HAPTIC FEEDBACK HERE
 
 #BACK TO RUNNING
+
+# After booting, motor buzzes twice
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(5, GPIO.OUT)
@@ -55,19 +62,19 @@ GPIO.output(5, GPIO.HIGH)
 time.sleep(0.5)
 GPIO.output(5, GPIO.LOW)
 
+# Main loop
 while True:
 	currentState = not GPIO.input(12)
 	if (currentState and not prevState):
-		held = True
+		jack.info("Button pressed once.")
 		picture = True
 		time.sleep(0.01)
 		endtime = time.time() + 1
 		while time.time() < endtime:
-			if (not currentState):
-				held = False
 			prevState = currentState
 			currentState = not GPIO.input(12)
 			if (currentState and not prevState):
+				jack.info("Button pressed twice.")
 				picture = False
 				time.sleep(0.01)
 				break
@@ -75,12 +82,14 @@ while True:
 			time.sleep(0.01)
 		if picture:
 			sub.call('echo "still" > /home/pi/pikrellcam/www/FIFO', shell=True)
+			jack.info("Image taken.")
 			GPIO.output(5, GPIO.HIGH)
 			time.sleep(0.5)
 			GPIO.output(5, GPIO.LOW)
 			time.sleep(1)
 		else:
 			sub.call('echo "record on 5 5" > /home/pi/pikrellcam/www/FIFO', shell=True)
+			jack.info("Video taken.")
 			GPIO.output(5, GPIO.HIGH)
 			time.sleep(0.25)
 			GPIO.output(5, GPIO.LOW)
@@ -89,7 +98,9 @@ while True:
 			time.sleep(0.25)
 			GPIO.output(5, GPIO.LOW)
 			time.sleep(5)
+	# Uploads backlog every 10 minutes
 	if time.time() >= backlogUploadTime + 600:
+		jack.info("Attempting to upload backlog.")
 		backlogUploadTime = time.time()
 		print 'Uploading backlog'
 		sub.call('python ./glimpse-cam/uploadBacklog.py &', shell=True)
