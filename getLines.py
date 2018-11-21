19fe334659e1b69e1e790661da54453c18bf3b26
jungo3 <38149377+jungo3@users.noreply.github.com>
2018-11-21 13:22:33 -0800

    Update getLines.py

--- getLines.py
+++ getLines.py
@@ -2,5 +2,5 @@ def retKey():
     lines = []
     with open("/home/pi/glimpse-cam/keys.txt") as f:
         for line in f:
-            lines.append(line.strip('\n'))
+            lines.append(line.strip())
     return lines
