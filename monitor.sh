4705a650bdfc6ce84e0edc7a4f00ef98ea43b420
jungo3 <38149377+jungo3@users.noreply.github.com>
2018-11-27 18:47:33 -0800

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -5,16 +5,16 @@ do
 	date >> battery.csv
 	hostname -I >> battery.csv
 	python -c "
-	import tinys3, socket, sys
-	sys.path.insert(0, '/home/pi/glimpse-cam')
-	import getLines as decode
-	A=decode.retKey()
-	conn=tinys3.Connection(A[0],A[1],tls=True,default_bucket='pi-5')
-	f=open('battery.csv','rb')
-	try:
-		conn.upload(socket.gethostname()+'/battery.csv',f)
-	except:
-		print 'Failed to connect'
+import tinys3, socket, sys
+sys.path.insert(0, '/home/pi/glimpse-cam')
+import getLines as decode
+A=decode.retKey()
+conn=tinys3.Connection(A[0],A[1],tls=True,default_bucket='pi-5')
+f=open('battery.csv','rb')
+try:
+	conn.upload(socket.gethostname()+'/battery.csv',f)
+except:
+	print 'Failed to connect'
 	"
 	sleep 60
 done
