69e68722fb9876ebbb792367b28f3f98c7e3ccd4
Justin <Justin>
2020-03-06 01:00:10 -0800

    Major refactor. Minor errors. Fixing.

--- GlimpseCam.py
+++ GlimpseCam.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python 
 import time, datetime, os, glob, socket, signal 
 import RPi.GPIO as GPIO 
 import subprocess as sub 
@@ -11,8 +10,11 @@ BUZZER_PIN = 5
 BUTTON_PIN = 12 
 LOGGER = None
 RECORD_BUTTON = None
+CAMERA = None
+SHUTDOWN = False
 
 def signal_handler(sig, frame):
+    SHUTDOWN = True # DO NOT REMOVE
     LOGGER.info("stopped by keyboard interrupt.")
     LOGGER.info("graceful exit.")
     GPIO.cleanup()
@@ -52,83 +54,74 @@ def buttonPressResponse():
     except:
         raise RuntimeError("buzz motor failure.")
 
-#BOOT TEST GOES HERE
-
-#print ("running camera test")
-#sub.call('echo "still" > /home/p7/pikrellcam/www/FIFO',shell=True)
-#time.sleep(1)
-#filename = rF.rename('/home/pi/pikrellcam/www/media/stills', 0)
-#time.sleep(30)
-#filepath = 's3://pi-1/' + socket.gethostname() + '/images/' + filename
-#try:
-#	sub.check_output(['aws', 's3', 'ls', filepath], shell=True)
-#except:
-#	print 'image file was not uploaded correctly'
-	#INSERT HAPTIC FEEDBACK HERE
-
-#IF IT REACHES HERE IT PASSED CAMERA
-#print ("running video test")
-#sub.call('echo "record on 5 5" > /home/pi/pikrellcam/www/FIFO',shell=True)
-#time.sleep(10)
-#filename = rF.rename('/home/pi/pikrellcam/www/media/videos', 0)
-#time.sleep(60)
-#filepath = 's3://pi-1/' + socket.gethostname() + '/videos/' + filename
-#try:
-#	sub.check_output(['aws', 's3', 'ls', filepath], shell=True)
-#except:
-#	print 'video file was not uploaded correctly'
-	#INSERT HAPTIC FEEDBACK HERE
-
-#BACK TO RUNNING
-
 def restartCamera():
     try: 
-        sub.check_call('/home/pi/pikrellcam/pikrellcam &', shell=True)
-        LOGGER.info("restart pikrellcam success.")
-        # TODO: remove this sleep
-        time.sleep(1)
-    except sub.CalledProcessError:
+        if CAMERA:
+            CAMERA.kill()
+            CAMERA = sub.Popen(['/home/pi/pikrellcam/pikrellcam'])
+            LOGGER.info("restart pikrellcam success.")
+        else:
+            LOGGER.error("pikrellcam restart called with no camera started.")
+    except OSError, ValueError:
         raise RuntimeError("restart pikrellcam failed.")
 
 
 # Raises RuntimeError
 def checkCamera():
-        try:
-            # check all processes
-            # TODO: remove PIPE
-            ps = sub.Popen(('ps','-A'), stdout=sub.PIPE)
+    try:
+        CAMERA.poll()
+        if CAMERA.returncode: 
+            LOGGER.error("pikrellcam is no longer running.")
             try:
-                    output = sub.check_output(('grep','pikrellcam'),stdin=ps.stdout)
-                    # TODO: what does this do?
-                    ps.wait()
-            except sub.CalledProcessError:
-                    LOGGER.error("pikrellcam is no longer running.")
-                    try:
-                        restartCamera()
-                    except RuntimeError:
-                        LOGGER.error(str(e))
-        except OSError, ValueError:
-            raise RuntimeError("failed to open process list.");
+                restartCamera()
+            except RuntimeError as e:
+                LOGGER.error(str(e))
+    except Exception as e:
+        LOGGER.error(str(e))
+        raise RuntimeError("failed to open process list.");
 
 def startCamera():
-        try:
-            # Start pikrellcam and directory watcher
-            # Scripts started in .bashrc
-            sub.check_call('/home/pi/pikrellcam/pikrellcam &', shell=True)
-            time.sleep(8)
-            LOGGER.info("pikrellcam start success.")
-        except sub.CalledProcessError:
-            LOGGER.error("pikrellcam start failed.")
+    try:
+        CAMERA = sub.Popen(['/home/pi/pikrellcam/pikrellcam'])
+        LOGGER.info("pikrellcam start success.")
+    except OSError, ValueError:
+        LOGGER.error("pikrellcam start failed.")
 
 # TODO: the "Main loop" code should be called by a signal listening
 # for the button press (GPIO.interrupt)
-# TODO: remove all shell=True for security and stability
 # Main loop
 # TODO; add in more specific exception handling so that only expected exceptions are handled,
 # we don't want to mask exceptions that we don't expect. Check out uploadFile.py for better
 # exception handling and logging patterns by always making calling function deal with exception.
 
+def triggerRecord():
+    try:
+        LOGGER.info("button pressed, starting record.")
+        buttonPressResponse()
+        record10()
+        LOGGER.info("record finished.")
+    except RuntimeError as e:
+        LOGGER.error(str(e))
+
+def setupCallbacks():
+    try:
+        RECORD_BUTTON.when_pressed = triggerRecord
+        LOGGER.info("setup callbacks success.")
+    except Exception as e:
+        LOGGER.error(str(e))
+        raise RuntimeError("setup callbacks failure.")
+
+def runCamera():
+    while not SHUTDOWN:
+        # just keep the camera running for now 
+        # and let the callbacks handle the rest
+        try:
+            checkCamera()
+        except RuntimeError as e:
+            LOGGER.error(str(e))
+
 if __name__=="__main__":
+    # setup
     try: 
         # Sets up log, if this fails we're screwed.
         logFormat='[%(asctime)s] {%(filename)s:%(lineno)d} %(levelname)s - %(message)s'
@@ -151,40 +144,24 @@ if __name__=="__main__":
 
         # setup the Buzzer
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        #GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
         GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
         # recognize Glimpse Cam Hardware
-        #RECORD_BUTTON = Button(BUTTON_PIN)
+        RECORD_BUTTON = Button(BUTTON_PIN)
 
         startCamera()
-        
+        setupCallbacks()
+
         # buzz motor for success
         buzzMotor2()
 
         LOGGER.info("setup success.")
-    except:
+    except Exception as e:
         LOGGER.error("setup failed.")
+        LOGGER.error(str(e))
         print(sys.exc_info()[0])
         raise
-    # camera handles all exceptions once running
-    while True:
-            buttonPressed = not GPIO.input(12)
-            # Always check the camera is running
-            try:
-                checkCamera()
-            except RuntimeError:
-                LOGGER.error(str(e))
-            # TODO: this condition is supposed to ensures a
-            # button press will not interrupt a button press that has
-            # happened, although the thread sleeps so it can never reach
-            # this state. Preserving for sanity... for now.
-            # if button pressed and not prevButtonPressed
-            if buttonPressed: 
-                LOGGER.info("button pressed, starting record.")
-                try:
-                    buttonPressResponse()
-                    record10()
-                except RuntimeError:
-                    LOGGER.error(str(e))
 
+    # run the camera
+    runCamera()
