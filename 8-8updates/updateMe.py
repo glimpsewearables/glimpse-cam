ac7c25efdfac20a13cda296c7d87e1c16359814a
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2020-08-09 21:12:31 -0700

    Update updateMe.py

--- 8-8updates/updateMe.py
+++ 8-8updates/updateMe.py
@@ -1,7 +1,11 @@
 from subprocess import call
+from shutil import copyfile
+#copyfile(src, dst)
 print "updating.."
-call.(["cp", "rc.local", "/etc/rc.local"])
-call.(["cp", "blink.service", "/lib/systemd/system/blink.service"])
+copyfile("/home/pi/nextGW/8-8updates/rc.local","/etc/rc.local")
+copyfile("/home/pi/nextGW/8-8updates/blink.service","/lib/systemd/system/blink.service")
+#call.(["cp", "rc.local", "/etc/rc.local"])
+#call.(["cp", "blink.service", "/lib/systemd/system/blink.service"])
 call.{["systemctl", "daemon-reload"]}
 time.sleep(3)
 call.(["systemctl", "enable", "blink.service"])
