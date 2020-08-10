fd18c0b005ca7b890de1034c338491f219a29c4a
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2020-08-09 21:13:44 -0700

    Update updateMe.py

--- 8-8updates/updateMe.py
+++ 8-8updates/updateMe.py
@@ -6,9 +6,9 @@ copyfile("/home/pi/nextGW/8-8updates/rc.local","/etc/rc.local")
 copyfile("/home/pi/nextGW/8-8updates/blink.service","/lib/systemd/system/blink.service")
 #call.(["cp", "rc.local", "/etc/rc.local"])
 #call.(["cp", "blink.service", "/lib/systemd/system/blink.service"])
-call.{["systemctl", "daemon-reload"]}
+call{["systemctl", "daemon-reload"]}
 time.sleep(3)
-call.(["systemctl", "enable", "blink.service"])
+call(["systemctl", "enable", "blink.service"])
 print "rebooting.."
 time.sleep(3)
-call.{["reboot"]}
+call{["reboot"]}
