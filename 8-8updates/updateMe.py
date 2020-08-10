f51790cb32c5bb985539d23f521b08a2a2a89c9b
glimpsewearables <38670739+glimpsewearables@users.noreply.github.com>
2020-08-09 21:09:00 -0700

    Update updateMe.py

--- 8-8updates/updateMe.py
+++ 8-8updates/updateMe.py
@@ -4,7 +4,7 @@ call.(["cp", "rc.local", "/etc/rc.local"])
 call.(["cp", "blink.service", "/lib/systemd/system/blink.service"])
 call.{["systemctl", "daemon-reload"]}
 time.sleep(3)
-call.(["systemctl", "enable blink.service"])
+call.(["systemctl", "enable", "blink.service"])
 print "rebooting.."
 time.sleep(3)
 call.{["reboot"]}
